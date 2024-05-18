237810d7920a7a549cf412a24772e53b834d52a8
EgorT <you@example.com>
2024-05-18 21:48:08 +0300

    add --format

--- gendiff/ref.py
+++ gendiff/ref.py
@@ -2,10 +2,11 @@ import argparse
 
 
 def show_reference():
-    parser = argparse.ArgumentParser(description='Compares two '
-                                                 'configuration files and '
-                                                 'shows a difference.', usage='gendiff'
-                                                 ' [-h] first_file second_file')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and '
+                    'shows a difference.',
+        usage='gendiff [-h] first_file second_file')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
