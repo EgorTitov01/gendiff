542466e325ba4861cd92afe42dff0508fb49236d
EgorT <you@example.com>
2024-05-18 21:29:04 +0300

    fix lint mistakes

--- gendiff/ref.py
+++ gendiff/ref.py
@@ -1,8 +1,10 @@
 import argparse
 
+
 def show_reference():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and '
-                                                 'shows a difference.',usage = 'gendiff'
+    parser = argparse.ArgumentParser(description='Compares two '
+                                                 'configuration files and '
+                                                 'shows a difference.', usage='gendiff'
                                                  ' [-h] first_file second_file')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
