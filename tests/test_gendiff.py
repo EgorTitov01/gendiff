1c5fae4ef3df338203d4b1557d597fe9eb0b8459
EgorT <you@example.com>
2024-06-03 23:14:57 +0300

    make 4th step

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,13 +5,7 @@ import json
 def test_not_empty():
     file1 = 'json_files/file1.json'
     file2 = 'json_files/file2.json'
-    result = json.dumps({
-    "- follow": False,
-    "host": "hexlet.io",
-    "- proxy": "123.234.53.22",
-    "- timeout": 50,
-    "+ timeout": 20,
-    "verbose": True
-}, indent=4)
+    result_file = 'tests/fixtures/result.json'
+    result = json.dumps(json.load(open(result_file)), indent=4)
 
     assert generate_diff(file1, file2) == result
